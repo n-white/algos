fe143ba4fa58905bd02f9b5a7209626a3d982ae5
n-white <neil.r.whitehead@gmail.com>
2016-10-15 16:18:49 -0700

    psudeo code for zero matrix problem

---
+++ Algorithms/Matrix/zero_matrix_CTCI-1.8.js
@@ -0,0 +1,15 @@
+// Write an algo such that if an element in an MxN matrix is 0, its entire row and column are set to 0
+
+
+var detectAndChange = function(matrix) {
+	// Keep two cache arrays to track rows and columns to change
+	var columns = [];
+	var rows = [];
+
+	// First iteration: find columns and rows to change
+	for (var)
+	// Second iteration: change rows to zeros
+	// Third iteration: change columns to zeros
+}
+
+// SOLUTION IS PRETTY EASY SO NOT ACTUALLY CODING IT
